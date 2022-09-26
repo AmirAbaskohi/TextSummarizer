adfcf09ab0c3bb1ee4c33f15e4d628b5827d4ff0
AmirHosseinAbaskohi <amirhossein.abaskohi@gmail.com>
2022-09-26 19:04:54 +0330

    Update bleu_scorer.py

--- eval/bleu/bleu_scorer.py
+++ eval/bleu/bleu_scorer.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Bleu scorer."""
-
 import collections
 import sacrebleu
 from rouge_score import scoring
